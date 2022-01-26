54b7378a6a07bdac8526bc9ee66d497a26bd7217
Mohit soni <88020997+Mohitson112@users.noreply.github.com>
2022-01-27 02:10:53 +0530

    Add files via upload

---
+++ Array/minmax.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+int arr[]={1,3,4,2,6};
+int n=5;
+int maxno=INT_MIN;
+int minno=INT_MAX;
+
+for (int  i = 0; i < n; i++)
+{
+    /* code */
+}
+
+return 0;
+}
